30d9cc887a9f959b5319eae00ac111e0605055c2
Ruhama Yared <ruhamayared1@gmail.com>
2022-12-30 17:08:33 -0500

    finished second api call getting a predicted age based on name

--- src/App.js
+++ src/App.js
@@ -3,31 +3,31 @@ import { useState, useEffect } from "react"
 import Axios from "axios"
 
 function App() {
-  //Fectching data normally in JS
-  // fetch("https://catfact.ninja/fact")
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log(data)
-  //   })
+  const [name, setName] = useState("")
+  const [predictedAge, setPredictedAge] = useState(null)
 
-  const [catFact, setCatFact] = useState("")
-
-  function fetchCatFact() {
-    Axios.get("https://catfact.ninja/fact").then((res) => {
-      setCatFact(res.data.fact)
+  function fetchData() {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data)
     })
   }
 
-  useEffect(() => {
-    fetchCatFact()
-  }, [])
-
   return (
     <div className="App">
-      <button className="btn" onClick={fetchCatFact}>
-        Generate Cat Fact 😸
+      <h1>
+        <input
+          placeholder="Name"
+          onChange={(event) => {
+            setName(event.target.value)
+          }}
+        />
+      </h1>
+      <button className="btn" onClick={fetchData}>
+        💃🏾 Predict Age 💃🏾
       </button>
-      <p>{catFact}</p>
+
+      <h3>Name: {predictedAge?.name}</h3>
+      <h3>Predicted Age: {predictedAge?.age}</h3>
     </div>
   )
 }
