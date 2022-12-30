372e6b8d2ad7bb927653b514b2b63f9ffd566d42
Ruhama Yared <ruhamayared1@gmail.com>
2022-12-30 13:13:00 -0500

    simple counter app

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import "./App.css"
 import { useState } from "react"
 
 function App() {
-  const [textColor, setTextColor] = useState("")
+  const [count, setCount] = useState(0)
+
+  function increase() {
+    setCount(count + 1)
+  }
+  function decrease() {
+    setCount(count - 1)
+  }
+  function setToZero() {
+    setCount(0)
+  }
 
   return (
     <div className="App">
-      <h1>
-        <button
-          onClick={() => {
-            setTextColor(textColor === "black" ? "red" : "black")
-          }}
-        >
-          Show/Hide
-        </button>
-      </h1>
-      <h1 style={{ color: textColor }}>Hi, my name is Ruhama.</h1>
+      <button onClick={increase}>Increase</button>
+      <button onClick={decrease}>Decrease</button>
+      <button onClick={setToZero}>Set to Zero</button>
+
+      <h1>{count}</h1>
     </div>
   )
 }
